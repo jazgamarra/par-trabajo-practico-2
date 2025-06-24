90a7231f2a52945c5e43c22bc3f99adccc75ff4f
jazgamarra <j444min@gmail.com>
2025-06-24 10:33:26 -0300

    CRUD de usuarios + arreglos visuales

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -2,6 +2,6 @@ from app import db
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50))
-    contrasenha = db.Column(db.String(50))
-    administrador = db.Column(db.Boolean)
+    nombre = db.Column(db.String(100), nullable=False)
+    contrasenha = db.Column(db.String(100), nullable=False)
+    administrador = db.Column(db.Boolean, default=False)
